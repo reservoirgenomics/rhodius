229caafca52100fad7c770953591bc48a11e9886
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2020-12-27 20:35:46 -0500

    Properly return sequence information as a singleres multivec

--- clodius/tiles/fasta.py
+++ clodius/tiles/fasta.py
@@ -1,15 +1,31 @@
 import math
+from typing import Any, List, Tuple
 
-from pydantic import BaseModel
+import numpy as np
 
 import clodius.tiles.chromsizes as cts
+from clodius.tiles.format import format_dense_tile
 from clodius.tiles.utils import TilesetInfo, abs2genome_fn, parse_tile_id
 from pysam import FastaFile
 
 TILE_SIZE = 1024
 
-class FastaTile(BaseModel):
-    seq: str
+def convert_bases_to_multivec(seq):
+    res = []
+
+    to_append = {
+        'a': [1, 0, 0, 0, 0, 0],
+        't': [0, 1, 0, 0, 0, 0],
+        'g': [0, 0, 1, 0, 0, 0],
+        'c': [0, 0, 0, 1, 0, 0],
+        'n': [0, 0, 0, 0, 1, 0],
+    }
+
+
+    for c in seq:
+        res.append(to_append.get(c.lower(), [0, 0, 0, 0, 0, 1]))
+    
+    return res;
 
 def tileset_info(fai_filename):
     """The tileset info of a fai file returns the tileset."""
@@ -17,15 +33,34 @@ def tileset_info(fai_filename):
     tsinfo["max_zoom"] = math.ceil(
         math.log(tsinfo["max_pos"][0] / TILE_SIZE) / math.log(2)
     )
+
     tsinfo["max_width"] = TILE_SIZE * 2 ** tsinfo["max_zoom"]
+    # tsinfo['bins_per_dimension'] = TILE_SIZE
+    tsinfo['tile_size'] = TILE_SIZE
+    tsinfo['datatype'] = 'multivec_singleres_sequence'
     return tsinfo
 
-def tiles(fasta_filename, fai_filename, tile_ids):
-    tsinfo = tileset_info(fai_filename)
+def tiles(fasta_filename: str, tile_ids: List[str],
+    index_filename: str, chromsizes_fn: str=None) -> List[Tuple[str, Any]]:
+    """Retrieve higlass tiles.
+
+    Arguments:
+        fasta_filename: The name of the fasta file to load
+        tile_ids: The incoming tile ids (e.g. 'x.0.0')
+        fai_filename: The name of the fasta index file (`samtools faidx`)
+        chromsizes_filename: The chromsizes filename to use in case we
+            want a specific chromosome order.
+    Returns:
+        Tile data
+    """
+    tsinfo = tileset_info(index_filename)
     tsinfo = TilesetInfo(**tsinfo)
     generated_tiles = []
 
-    fa_file = FastaFile(fasta_filename, fai_filename)
+    fa_file = FastaFile(fasta_filename, index_filename)
+
+    if not chromsizes_fn:
+        chromsizes_fn = index_filename
 
     for tile_id in tile_ids:
         tile_info = parse_tile_id(tile_id, tsinfo)
@@ -45,11 +80,14 @@ def tiles(fasta_filename, fai_filename, tile_ids):
         seq = ""
 
         for chr_interval in abs2genome_fn(
-            fai_filename, tile_info.start[0], tile_info.end[0]
+            chromsizes_fn, tile_info.start[0], tile_info.end[0]
         ):
             seq += fa_file.fetch(chr_interval.name, chr_interval.start, chr_interval.end)
 
-        tile = FastaTile(seq=seq)
-        generated_tiles += [(tile_id, tile.dict())]
+        res = convert_bases_to_multivec(seq)
+        tile = format_dense_tile(np.array(res).T)
+        tile['shape'] = [6, len(seq)]
+
+        generated_tiles += [(tile_id, tile)]
 
     return generated_tiles
