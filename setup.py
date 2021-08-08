6de688f6778fda4ab45eea35e996adbc02e44442
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2021-08-08 14:02:54 -0700

    Bump version: 0.27.0 → 0.28.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = get_requirements("requirements.txt")
 
 setup(
     name="clodius",
-    version="0.27.0",
+    version="0.28.0",
     description="Tile generation for big data",
     author="Peter Kerpedjiev",
     author_email="pkerpedjiev@gmail.com",
